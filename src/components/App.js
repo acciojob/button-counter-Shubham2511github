70b189883feb94d21c8180c778fc1dfc028929d8
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-07-15 19:49:53 +0000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
   }
   return (
     <div>
-        <p>Button clicked {count} times </p>
+        <p>Button clicked {count} times .</p>
         <button onClick={increaseCount}>Click me</button>
     </div>
   )
